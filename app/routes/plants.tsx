6c8491461189c43518c6209f8f8b7e56b3664e8d
kyher <kyle150896@gmail.com>
2022-05-06 08:17:45 +0100

    Further styling updates.
    More green + remove email from header nav when logged in.

--- app/routes/plants.tsx
+++ app/routes/plants.tsx
@@ -22,15 +22,14 @@ export default function PlantsPage() {
 
   return (
     <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+      <header className="flex items-center justify-between bg-green-800 p-4 text-white">
         <h1 className="text-3xl font-bold">
           <Link to=".">Plants</Link>
         </h1>
-        <p>{user.email}</p>
         <Form action="/logout" method="post">
           <button
             type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            className="rounded bg-green-600 py-2 px-4 text-blue-100 hover:bg-green-500 active:bg-green-400"
           >
             Logout
           </button>
@@ -39,7 +38,7 @@ export default function PlantsPage() {
 
       <main className="flex h-full bg-white">
         <div className="h-full w-80 border-r bg-gray-50">
-          <Link to="new" className="block p-4 text-xl text-blue-500">
+          <Link to="new" className="block p-4 text-xl text-green-500">
             + New plant
           </Link>
 

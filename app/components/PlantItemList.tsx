0c5bec7ee22800930f77010aaa8ae860b432a5f4
kyher <kyle150896@gmail.com>
2022-05-27 09:09:55 +0100

    Added PlantItemList component

---
+++ app/components/PlantItemList.tsx
@@ -0,0 +1,25 @@
+import { getFeedingListItems } from "~/models/feeding.server";
+import { getWateringListItems } from "~/models/watering.server";
+import { PlantItem } from "./PlantItem";
+
+type props = {
+  type: string;
+  list:
+    | Awaited<ReturnType<typeof getWateringListItems>>
+    | Awaited<ReturnType<typeof getFeedingListItems>>;
+};
+
+export const PlantItemList = ({ type, list }: props) => {
+  return (
+    <div className="mr-5 rounded px-5 py-2 lg:w-1/3">
+      <p>
+        <strong>{type}:</strong>{" "}
+      </p>
+      <ul>
+        {list.length
+          ? list.map(({ id, date }) => <PlantItem id={id} date={date} />)
+          : `No ${type.toLowerCase()} logged for this plant.`}
+      </ul>
+    </div>
+  );
+};
